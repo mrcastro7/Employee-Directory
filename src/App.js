19a43a715cf354a0f8920e495be87e6ca77bcbca
Marvin Castro <bbmarvin2003@yahoo.com>
2020-04-30 11:53:29 -0400

    adding wrapper header main comp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>Test App </p>
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import DataArea from "./DataArea";
+// import "../styles/Main.css"; create if i want to style further
+
+function Main () {
+    return (
+      <>
+        {/* <DataArea /> */}
+      </>
+    );
+  };
+
+export default Main;
